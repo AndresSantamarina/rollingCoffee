58b6030abd3d0c301c2411f53ba046cd1ed7b8f3
Andres Santamarina <andresesantamarina@gmail.com>
2024-02-07 11:35:11 -0300

    agregue el navbar

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,10 +1,21 @@
-
-
+import { Navbar, Nav, Container } from "react-bootstrap";
+import logo from "../../assets/coffee_Logo.png"
 const Menu = () => {
     return (
-        <div>
-            <p>Aquí agregar el maquetado del menú</p>
-        </div>
+        <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home"><img src={logo} alt="logo de RollingCoffee" className="img-fluid" width={150}/></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Inicio</Nav.Link>
+            <Nav.Link href="#link">Administrador</Nav.Link>
+            <Nav.Link href="#link">Registro</Nav.Link>
+            <Nav.Link href="#link">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     );
 };
 
