a9f04aba548e9ae506164878fcf9bda3c5d2e581
Andres Santamarina <andresesantamarina@gmail.com>
2024-02-20 09:38:51 -0300

    agregue las validaciones en FormularioProducto usando react hook form

--- src/components/pages/producto/FormularioProducto.jsx
+++ src/components/pages/producto/FormularioProducto.jsx
@@ -36,49 +36,118 @@ const FormularioProducto = () => {
               },
             })}
           />
-          <Form.Text className="text-danger">{errors.nombreProducto?.message}</Form.Text>
+          <Form.Text className="text-danger">
+            {errors.nombreProducto?.message}
+          </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formPrecio">
           <Form.Label>Precio*</Form.Label>
-          <Form.Control type="number" placeholder="Ej: 50" />
-          <Form.Text className="text-danger">prueba de error</Form.Text>
+          <Form.Control
+            type="number"
+            placeholder="Ej: 50"
+            {...register("precio", {
+              required: "El precio del producto es obligatorio",
+              min: {
+                value: 100,
+                message:
+                  "El precio del producto debe de ser como mínimo de $100",
+              },
+              max: {
+                value: 2000,
+                message:
+                  "El precio del producto debe de ser como máximo de $2000",
+              },
+            })}
+          />
+          <Form.Text className="text-danger">
+            {errors.precio?.message}
+          </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formImagen">
           <Form.Label>Imagen URL*</Form.Label>
           <Form.Control
             type="text"
             placeholder="Ej: https://www.pexels.com/es-es/vans-en-blanco-y-negro-fuera-de-la-decoracion-para-colgar-en-la-pared-1230679/"
+            {...register("imagen", {
+              required: "La URL de la imagen es obligatoria",
+              minLength: {
+                value: 5,
+                message:
+                  "La URL de la imagen debe de ser mayor a los 5 caracteres",
+              },
+              maxLength: {
+                value: 500,
+                message:
+                  "La URL de la imagen debe de ser menor a los 500 caracteres",
+              },
+            })}
           />
-          <Form.Text className="text-danger">prueba de error</Form.Text>
+          <Form.Text className="text-danger">
+            {errors.imagen?.message}
+          </Form.Text>
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formPrecio">
+        <Form.Group className="mb-3" controlId="formCategoria">
           <Form.Label>Categoría*</Form.Label>
-          <Form.Select>
+          <Form.Select {...register("categoria",{
+            required: "Seleccione una opción válida"
+          })}>
             <option value="">Seleccione una opcion</option>
             <option value="Infusiones">Infusiones</option>
             <option value="Batidos">Batidos</option>
             <option value="dulce">Dulce</option>
             <option value="salado">Salado</option>
           </Form.Select>
-          <Form.Text className="text-danger">prueba de error</Form.Text>
+          <Form.Text className="text-danger">
+            {errors.categoria?.message}
+          </Form.Text>
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formImagen">
+        <Form.Group className="mb-3" controlId="formDescripcionBreve">
           <Form.Label>Descripción breve*</Form.Label>
           <Form.Control
             type="text"
             placeholder="Ej: Una taza de café suave y aromático."
             as="textarea"
+            {...register("descripcionBreve", {
+              required: "La descripción breve del producto es obligatoria",
+              minLength: {
+                value: 20,
+                message:
+                  "La descripción breve del producto debe tener como mínimo 20 caracteres",
+              },
+              maxLength: {
+                value: 100,
+                message:
+                  "La descripción breve del producto debe tener como máximo 100 caracteres",
+              },
+            })}
           />
-          <Form.Text className="text-danger">prueba de error</Form.Text>
+          <Form.Text className="text-danger">
+            {errors.descripcionBreve?.message}
+          </Form.Text>
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formImagen">
+        <Form.Group className="mb-3" controlId="formDescripcionAmplia">
           <Form.Label>Descripción Amplia*</Form.Label>
           <Form.Control
             type="text"
             placeholder="Ej: El café americano es una bebida caliente que consiste en un espresso diluido con agua caliente, lo que resulta en una taza de café suave y aromático. Es una opción popular para aquellos que prefieren un café menos intenso que el espresso tradicional. Perfecto para disfrutar en cualquier momento del día."
             as="textarea"
+            {...register("descripcionAmplia", {
+              required: "La descripción amplia del producto es obligatorio",
+              minLength: {
+                value: 150,
+                message:
+                  "La descripción amplia del producto debe tener como mínimo 150 caracteres",
+              },
+              maxLength: {
+                value: 600,
+                message:
+                  "La descripción amplia del producto debe tener como máximo 600 caracteres",
+              },
+            })}
           />
-          <Form.Text className="text-danger">prueba de error</Form.Text>
+          <Form.Text className="text-danger">
+            {errors.descripcionAmplia?.message}
+          </Form.Text>
         </Form.Group>
 
         <Button type="submit" variant="success">
